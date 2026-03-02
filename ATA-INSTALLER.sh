9db4da18132a74b3cc921279b189c852e3d9be4f
silentShulk <marcostrada258@gmail.com>
2026-03-02 23:14:43 +0100

    Finalized installation script

--- ATA-INSTALLER.sh
+++ ATA-INSTALLER.sh
@@ -2,32 +2,41 @@
 
 # CHECK FOR ARGUMENT
 if [ $# -eq 0 ]; then
-  echo "REQUIRES ARGUMENT NOT FOUND.\n
-  Run the installer again and pass the path to Automata's folder\n
+  echo "REQUIRED ARGUMENT NOT FOUND.
+  Run the installer again and pass the path to Automata's folder
   (the one containing the exe)";
   exit 1
 fi
 
 
 
-# Paths
-$data_folder = $HOME/.config/ATA/
-$files_folder = $HOME/.local/share/ATA
-$bin_folder = /usr/bin
-
-
-
 # Creating directories
-mkdir -p $data_folder
-mkdir -p $files_folder
+mkdir -p $HOME/.config/ATA/
+mkdir -p $HOME/.local/share/ATA
+mkdir -p $HOME/.local/bin
 
 
 
 # Copying files into the newly created directories
-cp ./install-prerequisites.sh $files_folder
-# Copy executable into $bin_folder
+echo "Copying ATA files into ~/.local/share/ and ~/usr/bin"
+cp ./install-prerequisites.sh $HOME/.local/share/ATA
+cp ./target/release/ATA $HOME/.local/bin
 
 
 
 # Creating default data file
+echo "Creating default data file in ~/.config/"
+touch $HOME/.config/data.json
 
+cat <<EOF > "$HOME/.config/ATA/data.json"
+{
+  "game_path": "$1",
+  "mods": []
+}
+EOF
+
+
+
+echo ""
+echo "Installation complete, make sure ~/.local/bin is in your PATH
+Do not touch the ATA folders in ~/.config ~/.local/share and ~/.local/bin"
