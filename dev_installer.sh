fc5c8bd93e1675f6c2e1a90ba3f967778574143e
silentShulk <marcostrada258@gmail.com>
2026-05-28 17:17:44 +0200

    Added developer script

---
+++ dev_installer.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+rm -rf ~/.local/share/Steam/steamapps/common/NieRAutomata/data/pl
+rm -rf ~/.local/share/Steam/steamapps/common/NieRAutomata/data/wp
+rm -rf ~/.local/share/Steam/steamapps/common/NieRAutomata/data/bg
+
+mkdir -p ~/.local/share/Steam/steamapps/common/NieRAutomata/data
+mkdir -p ~/.local/share/Steam/steamapps/common/NieRAutomata/wax/mods
+
+
+
+mkdir -p ~/.config/ATA/ && mkdir -p ~/.local/share/ATA
+touch ~/.local/share/ATA/data.json ~/.config/ATA/settings.json
+
+
+
+cat << 'EOF' > ~/.local/share/ATA/data.json
+{
+    "mods": []
+}
+EOF
+
+cat << 'EOF' > ~/.config/ATA/settings.json
+{
+  "style": "SilentShulk",
+  "palette": "Replicant",
+  "sortingOrder": "ModType",
+  "filesConflictResolution": "Ask",
+  "keepExtractedFolders": true,
+  "extractedFoldersLocation": "/home/cmarco/Downloads/",
+  "gamePath": "/home/cmarco/.local/share/Steam/steamapps/common/NieRAutomata/",
+  "discordRichPresence": "Altering NieRAutomata's timelines"
+}
+EOF
