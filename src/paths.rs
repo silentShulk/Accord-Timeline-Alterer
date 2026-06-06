64fc3f34a78ec7524d2ec18d20dac197fc8698a3
silentShulk <marcostrada258@gmail.com>
2026-06-06 21:52:59 +0200

    Updated paths for Linux and windows

--- src/paths.rs
+++ src/paths.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 use std::sync::LazyLock;
-use dirs::{home_dir, data_dir, config_local_dir, state_dir, data_local_dir};
+use dirs::{home_dir, config_dir, state_dir, data_local_dir};
 
 pub struct Paths {
     pub executable: PathBuf,
@@ -11,27 +11,16 @@ pub struct Paths {
 }
 
 pub static PATHS: LazyLock<Paths> = LazyLock::new(|| {
-    #[cfg(target_os = "linux")]
     return Paths {
+        #[cfg(target_os = "linux")]
         executable: home_dir().unwrap()
             .join(".local").join("bin").join("ATA").join("ATA"),
-        data_file: data_dir().unwrap()
-            .join("ATA").join("data.json"),
-        settings_file: config_local_dir().unwrap()
-            .join("ATA").join("settings.json"),
-        uis_dir: state_dir().or_else(data_local_dir).unwrap()
-            .join("ATA").join("UIs"),
-        apps_dir: state_dir().or_else(data_local_dir).unwrap()
-            .join("ATA").join("Apps"),
-    };
-
-    #[cfg(target_os = "windows")]
-    return Paths {
+        #[cfg(target_os = "windows")]
         executable: data_local_dir().unwrap()
             .join("Programs").join("ATA").join("ATA.exe"),
-        data_file: data_dir().unwrap()
+        data_file: data_local_dir().unwrap()
             .join("ATA").join("data.json"),
-        settings_file: config_local_dir().unwrap()
+        settings_file: config_dir().unwrap()
             .join("ATA").join("settings.json"),
         uis_dir: data_local_dir().unwrap()
             .join("ATA").join("UIs"),
