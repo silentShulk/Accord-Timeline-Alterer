f2e23cc1098b0b4ac3f46b2a081a172c0ceac3b1
silentShulk <marcostrada258@gmail.com>
2026-06-06 23:23:39 +0200

    Made usefull ashustments to ps1 dev script

--- dev_installer.ps1
+++ dev_installer.ps1
@@ -2,17 +2,27 @@
 
 # Remove folders for mod files (will be recreated by ATA if necessary)
 # This doesn't affect a working installation of the game
-rm -r -f "$HOME/.local/share/Steam/steamapps/common/NieRAutomata/data/pl"
-rm -r -f "$HOME/.local/share/Steam/steamapps/common/NieRAutomata/data/wp"
-rm -r -f "$HOME/.local/share/Steam/steamapps/common/NieRAutomata/data/bg"
-rm -r -f "$HOME/.local/share/Steam/steamapps/common/NieRAutomata/data/wax"
+$modPaths = @(
+    "$HOME/.local/share/Steam/steamapps/common/NieRAutomata/data/pl",
+    "$HOME/.local/share/Steam/steamapps/common/NieRAutomata/data/wp",
+    "$HOME/.local/share/Steam/steamapps/common/NieRAutomata/data/bg",
+    "$HOME/.local/share/Steam/steamapps/common/NieRAutomata/data/wax"
+)
+
+foreach ($path in $modPaths) {
+    if (Test-Path $path) {
+        # This will only run if the path exists. Else error gets thrown polluting the output
+        # If it fails due to permissions, it WILL throw a visible error.
+        rm -r -f $path
+    }
+}
 
 
 
 # Create folders strictly necessary for development testing
 # With these development testing is possible even if the game isn't installed
-mkdir "$HOME/.local/share/Steam/steamapps/common/NieRAutomata/data" -Force
-mkdir "$HOME/.local/share/Steam/steamapps/common/NieRAutomata/wax/mods" -Force
+mkdir "$HOME/.local/share/Steam/steamapps/common/NieRAutomata/data" -Force | Out-Null
+mkdir "$HOME/.local/share/Steam/steamapps/common/NieRAutomata/wax/mods" -Force | Out-Null
 
 
 
@@ -23,11 +33,11 @@ $settings = "$HOME/.config/ATA"
 $uis = "$HOME/.local/share/UIs"
 $apps = "$HOME/.local/share/Apps"
 
-mkdir $exe -Force
-mkdir $data -Force
-mkdir $settings -Force
-mkdir $uis -Force
-mkdir $apps -Force
+mkdir $exe -Force | Out-Null
+mkdir $data -Force | Out-Null
+mkdir $settings -Force | Out-Null
+mkdir $uis -Force | Out-Null
+mkdir $apps -Force | Out-Null
 
 
 
