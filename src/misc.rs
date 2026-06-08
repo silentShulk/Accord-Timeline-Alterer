3b88734b7dc6821c40ccd99f82d79d32fcff5a1e
silentShulk <marcostrada258@gmail.com>
2026-06-08 16:53:44 +0200

    Removed warning from temp implementation of discord RP

--- src/misc.rs
+++ src/misc.rs
@@ -1,72 +1,72 @@
-use std::sync::LazyLock;
+// use std::sync::LazyLock;
 
 use std::process::Command;
 
-use std::time::{SystemTime, UNIX_EPOCH};
+// use std::time::{SystemTime, UNIX_EPOCH};
 
-use discord_rich_presence::{DiscordIpc, DiscordIpcClient, activity::Activity, activity::Timestamps};
+// use discord_rich_presence::{DiscordIpc, DiscordIpcClient, activity::Activity, activity::Timestamps};
 
-use thiserror::Error;
-
-
+// use thiserror::Error;
 
 
-#[derive(Error, Debug)]
-pub enum DiscordError {
-    #[error("There was an error connecting to discord's IPC. {0}")]
-    ClientConnection(#[from] discord_rich_presence::error::Error),
 
-    #[error("Couldn't set the user's activity on discord")]
-    ActivitySetting(discord_rich_presence::error::Error)
-}
 
-#[derive(Debug, Copy, Clone)]
-pub enum Action {
-    JustOpened,
-    Installing,
-    Uninstalling,
-    Enabling,
-    Disabling,
-    ListingMods,
-    ChangingSettings,
-    Playing
-}
-impl AsRef<str> for Action {
-    fn as_ref(&self) -> &str {
-        match self {
-            Action::JustOpened => "Woke up silly...",
-            Action::Installing => "Mixing universes together",
-            Action::Uninstalling => "Unaltering Timelines",
-            Action::Enabling => "Bringing back the chaos",
-            Action::Disabling => "Taking a step back from the chaos",
-            Action::ListingMods => "Taking a look at the mess they've made",
-            Action::ChangingSettings => "Not-so-carefully playing with the cosmic dials",
-            Action::Playing => "Enjoying their custom-made fan-fiction"
-        }
-    }
-}
+// #[derive(Error, Debug)]
+// pub enum DiscordError {
+//     #[error("There was an error connecting to discord's IPC. {0}")]
+//     ClientConnection(#[from] discord_rich_presence::error::Error),
 
-static START_TIME: LazyLock<i64> = LazyLock::new(|| {
-    SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_secs() as i64
-});
-const ATA_ID: &str = "1512579862676639864";
+//     #[error("Couldn't set the user's activity on discord")]
+//     ActivitySetting(discord_rich_presence::error::Error)
+// }
 
-pub fn update_discord_rich_presence(drp: &String, last_action: Action) -> Result<(), DiscordError> {
-    let mut client = DiscordIpcClient::new(ATA_ID);
-    client.connect().map_err(|er| DiscordError::ClientConnection(er))?;
+// #[derive(Debug, Copy, Clone)]
+// pub enum Action {
+//     JustOpened,
+//     Installing,
+//     Uninstalling,
+//     Enabling,
+//     Disabling,
+//     ListingMods,
+//     ChangingSettings,
+//     Playing
+// }
+// impl AsRef<str> for Action {
+//     fn as_ref(&self) -> &str {
+//         match self {
+//             Action::JustOpened => "Woke up silly...",
+//             Action::Installing => "Mixing universes together",
+//             Action::Uninstalling => "Unaltering Timelines",
+//             Action::Enabling => "Bringing back the chaos",
+//             Action::Disabling => "Taking a step back from the chaos",
+//             Action::ListingMods => "Taking a look at the mess they've made",
+//             Action::ChangingSettings => "Not-so-carefully playing with the cosmic dials",
+//             Action::Playing => "Enjoying their custom-made fan-fiction"
+//         }
+//     }
+// }
 
-    let payload = Activity::new()
-        .details(drp)
-        .state(last_action.as_ref())
-        .timestamps(Timestamps::new().start(*START_TIME));
+// static START_TIME: LazyLock<i64> = LazyLock::new(|| {
+//     SystemTime::now()
+//         .duration_since(UNIX_EPOCH)
+//         .unwrap()
+//         .as_secs() as i64
+// });
+// const ATA_ID: &str = "1512579862676639864";
 
-    client.set_activity(payload).map_err(|er| DiscordError::ActivitySetting(er))?;
+// pub fn update_discord_rich_presence(drp: &String, last_action: Action) -> Result<(), DiscordError> {
+//     let mut client = DiscordIpcClient::new(ATA_ID);
+//     client.connect().map_err(|er| DiscordError::ClientConnection(er))?;
+
+//     let payload = Activity::new()
+//         .details(drp)
+//         .state(last_action.as_ref())
+//         .timestamps(Timestamps::new().start(*START_TIME));
+
+//     client.set_activity(payload).map_err(|er| DiscordError::ActivitySetting(er))?;
     
-    Ok(())
-}
+//     Ok(())
+// }
 
 pub fn launch_automata() -> Result<(), std::io::Error> {
     let app_id = "524220";
